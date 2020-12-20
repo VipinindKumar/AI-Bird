1f14b42a6c8f39e766146576c6ad280b22d281f7
Vipin Kumar <vipin.ind.kumar@gmail.com>
2020-12-20 18:15:29 +0530

    create object class and it's init method

--- gameAI.py
+++ gameAI.py
@@ -9,3 +9,14 @@ HEIGHT = 800
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game AI")
+
+
+class copter:
+	"""docstring for copter"""
+	def __init__(self, x, y):
+		''' initialize the copter object and
+			sets positional param '''
+		self.x, self.y = x, y
+		self.velocity = 0
+		self.height = self.y
+		# self.img = pygame.draw.circle(screen, BLUE, pos, 20)
