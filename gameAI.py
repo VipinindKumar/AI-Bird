0883f3e943bedca0c24fd86cf45687bd4af9501d
Vipin Kumar <vipin.ind.kumar@gmail.com>
2020-12-20 17:37:21 +0530

    create window with pygame

---
+++ gameAI.py
@@ -0,0 +1,11 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIDTH = 600
+HEIGHT = 800
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game AI")
