b63bbd85587452e8dac908ce0d4eb42e4c64b86b
Vipin Kumar <vipin.ind.kumar@gmail.com>
2020-12-21 09:01:02 +0530

    add code to draw copter

--- gameAI.py
+++ gameAI.py
@@ -21,11 +21,27 @@ class copter:
 		self.height = self.y
 		# self.img = pygame.draw.circle(screen, BLUE, pos, 20)
 
+	def draw(self):
+		''' draw the copter on window '''
+
+		myimage = pygame.image.load("img/copter.png")
+		imagerect = myimage.get_rect()
+
+
+		win.blit(myimage, imagerect)
+		# while 1:
+		#     your_code_here
+
+		#     screen.fill(black)
+		#     pygame.display.flip()
+
 	def move(self):
 		''' move the copter '''
 
+
 run = True
 while run:
+	# to stop execution press 'q' on keyboard
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_q:
