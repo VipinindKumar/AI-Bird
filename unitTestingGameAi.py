34884002b94175aee1682d418bc20212d1e5c624
Vipin Kumar <vipin.ind.kumar@gmail.com>
2021-01-07 10:25:22 +0530

    test jump function of the copter

--- unitTestingGameAi.py
+++ unitTestingGameAi.py
@@ -1,4 +1,5 @@
 import unittest
+import gameAI
 
 class testGame(unittest.TestCase):
 	def setUP(self):
@@ -6,3 +7,14 @@ class testGame(unittest.TestCase):
 		self.heli2 = copter(500, 600)
 		self.heli3 = copter(250, 300)
 
+	def test_jump(self):
+		self.heli1.jump()
+		self.assertEual(self.heli1.velocity, -10)
+		self.assertEual(self.heli1.tick_cnt, 0)
+
+		self.heli2.jump()
+		self.assertEual(self.heli2.velocity, -10)
+		self.assertEual(self.heli2.tick_cnt, 0)
+
+	def test_move(self):
+		pass
